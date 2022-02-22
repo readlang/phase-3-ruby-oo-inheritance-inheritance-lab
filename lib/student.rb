2a48b1fdc1c55eb2a6a028653fff53f6232712a9
Read Langworthy <rlangworthy@gmail.com>
2022-02-22 12:04:56 -0800

    commit tree 311fbaecb7f8673a11aeda0f2aa79ff40b093ba6

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative './user'
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    
+
+    def learn(string)
+        self.knowledge.push(string)
+    end
+
+    # def knowledge
+    #     self.knowledge
+    # end
 
 end
